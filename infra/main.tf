f07b03ec19604444796ea35bd757dcc3f337c14e
Wai Liu <waiholiu@users.noreply.github.com>
2024-03-07 10:50:15 +1100

    Update logic app resource group and storage account

--- infra/main.tf
+++ infra/main.tf
@@ -21,44 +21,34 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup2"
-  location = "westus2"
+resource "azurerm_resource_group" "logic_app_rg" {
+  name     = "todellogicapps-rg"
+  location = "Australia East"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "azure-functions-test-rg"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "example" {
-  name                     = "functionsapptestsawai"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+resource "azurerm_storage_account" "todellogicappsstorageacc" {
+  name                     = "todellogicappsstorageacc"
+  resource_group_name      = azurerm_resource_group.logic_app_rg.name
+  location                 = azurerm_resource_group.logic_app_rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "azure-functions-test-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  kind                = "elastic"
-
-
-  sku {
-    tier = "WorkflowStandard"
-    size = "WS1"
-  }
+resource "azurerm_service_plan" "todellogicappsasp" {
+  name                = "todellogicappsasp"
+  resource_group_name = azurerm_resource_group.logic_app_rg.name
+  location            = azurerm_resource_group.logic_app_rg.location
+  os_type             = "Windows"
+  sku_name            = "WS1"
 }
 
-resource "azurerm_logic_app_standard" "example" {
-  name                       = "test-azure-functions"
-  location                   = azurerm_resource_group.example.location
-  resource_group_name        = azurerm_resource_group.example.name
-  app_service_plan_id        = azurerm_app_service_plan.example.id
-  storage_account_name       = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+resource "azurerm_logic_app_standard" "todellogicappsasp" {
+  name                       = "todellogicappsasp"
+  location                   = azurerm_resource_group.logic_app_rg.location
+  resource_group_name        = azurerm_resource_group.logic_app_rg.name
+  app_service_plan_id        = azurerm_service_plan.todellogicappsasp.id
+  storage_account_name       = azurerm_storage_account.todellogicappsstorageacc.name
+  storage_account_access_key = azurerm_storage_account.todellogicappsstorageacc.primary_access_key
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME"     = "node"
