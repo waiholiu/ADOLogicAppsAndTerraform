205f3f5cdcedc1d3feafcc729e1554657e98e940
Wai Liu <waiholiu@users.noreply.github.com>
2024-03-07 09:35:40 +1100

    Add Azure backend configuration for Terraform state storage

--- infra/main.tf
+++ infra/main.tf
@@ -7,6 +7,13 @@ terraform {
     }
   }
 
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate12909"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
   required_version = ">= 1.1.0"
 }
 
