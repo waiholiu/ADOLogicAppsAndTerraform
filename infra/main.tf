da977580281311f46c16a7dfafe3ff80b4b51afb
Wai Liu <waiholiu@users.noreply.github.com>
2024-03-07 09:49:15 +1100

    Add Azure resources for Azure Functions test

--- infra/main.tf
+++ infra/main.tf
@@ -25,3 +25,43 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup2"
   location = "westus2"
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "azure-functions-test-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = "functionsapptestsawai"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "azure-functions-test-service-plan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "elastic"
+
+
+  sku {
+    tier = "WorkflowStandard"
+    size = "WS1"
+  }
+}
+
+resource "azurerm_logic_app_standard" "example" {
+  name                       = "test-azure-functions"
+  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.example.name
+  app_service_plan_id        = azurerm_app_service_plan.example.id
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"     = "node"
+    "WEBSITE_NODE_DEFAULT_VERSION" = "~18"
+  }
+}
